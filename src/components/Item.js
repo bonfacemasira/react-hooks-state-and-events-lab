6f5406080b1cc15e98c00c6d142469d0a37581ae
Bonface Masira <bonfacemasira@gmail.com>
2022-07-19 02:01:56 +0300

    commit tree 1b60be29d96ff8c8a10cfc0e43065269f5c0ea4d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [inCart, setInCart] = useState(false);
+  const listItems = inCart ? "in-cart" : "";
+
+  const displayText = inCart ? "Remove From Cart" : "Add to Cart";
+
+  function addToCart() {
+    setInCart(!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={listItems}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{displayText}</button>
     </li>
   );
 }
